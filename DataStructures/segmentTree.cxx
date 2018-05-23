4a4a6257a49cca6092bd8189f4d0615e963a69a0
skylarkgit <abhay199658@gmail.com>
2018-05-23 07:16:27 +0000

    segment trees, find bugs

--- DataStructures/segmentTree.cxx
+++ DataStructures/segmentTree.cxx
@@ -1,48 +1,110 @@
 #include<iostream>
 #include<bits/stdc++.h>
+using namespace std;
 
 namespace skylarkgit{
 	#define llu long long unsigned	
 	#define PENDING 	true
 	#define COMPLETE	false
+	#define ST_left(x)	((x)<<1)
+	#define ST_right(x)	(((x)<<1)|1)
+	#define ST_leftRangeLeft(x,y)	(x)
+	#define ST_leftRangeRight(x,y)	(x+((y-x)>>1))
+	#define ST_rightRangeLeft(x,y)	(x+((y-x)>>1)+1)
+	#define ST_rightRangeRight(x,y)	(y)
+
 	template <typename T>
 	class segmentTree{
 
 	protected:
-		size_t 		root;
 		size_t 		maxSize,maxUserSize;
 		size_t 		bitLength;
-		vector<T> 	treeSpace;
-		vector<T> 	status;
-	
+		std::vector<T> 	treeSpace;
+		std::vector<T> 	status;
+		T&			pushKernel(T &t,size_t lIdx,size_t rIdx,size_t lLimit,size_t rLimit,size_t index,T (*f)(T &currNode,T &thisNode));
+		T			getKernel(size_t lIdx,size_t rIdx,size_t lLimit,size_t rLimit,size_t index,T (*f)(T &lNode,T &rNode));
 	public:
 				segmentTree(size_t size);
-		void 	push(T &t,size_t a,size_t b,T (*f)(T &left,T &right),size_t leftIndex=0,size_t rightIndex=maxUserSize);
-		T& 		get	(size_t a,size_t b);
+		void 	push(T &t,size_t a,size_t b,T (*f)(T &currNode,T &thisNode));
+		T 		get	(size_t a,size_t b,T (*f)(T &currNode,T &thisNode));
 
 	};
 
 	template <typename T>
 	segmentTree<T>::segmentTree(size_t size){
 		maxUserSize=--size;
+		maxSize=0;
 		bool flag=false;
 		bitLength=8*sizeof(size);
-		size_t mask=(1<<(bitLength-1));
+		size_t mask=1;
+		(mask<<=(bitLength-1));
+		//cout<<bitset<64>(mask>>60)<<endl;
 		while(bitLength--){
 			if(mask&size) flag=true;
 			if(flag) ((maxSize<<=1)|=1);
 			(size<<=1);
+			//cout<<"bl="<<bitLength<<" maxSize="<<maxSize<<endl;
 		}
 		treeSpace.resize(((maxSize<<1)|1));	//2n+1
-		root=((maxSize<<1)|1)-1;
 	}
 
 	template <typename T>
-	void segmentTree<T>::push(T &t,size_t n,T (*f)(T &left,T &right)){
-		size_t node=n,mask=(1<<(bitLength-1));
-		treeSpace[n]=t;
-		while(node!=root){
-			node=mask|(node>>1);
-
-		}
+	void segmentTree<T>::push(T &t,size_t a,size_t b,T (*f)(T &left,T &right)){
+		pushKernel(t,a,b,0,maxUserSize,1,f);
 	}
+
+	template <typename T>
+	T segmentTree<T>::get(size_t a,size_t b,T (*f)(T &currNode,T &thisNode)){
+		treeSpace[0]=getKernel(a,b,0,maxUserSize,1,f);
+		return treeSpace[0];
+	}
+
+	//***********************KERNEL IMPLEMENTATION***************************//
+	template <typename T>
+	T& segmentTree<T>::pushKernel(T &t,size_t lIdx,size_t rIdx,size_t lLimit,size_t rLimit,size_t index,T (*f)(T &currNode,T &thisNode)){
+		//if(lIdx<=lLimit&&rIdx>=rLimit)
+		//cout<<" lIdx="<<lIdx<<" rIdx="<<rIdx<<" lLimit="<<lLimit<<" rLimit="<<rLimit<<" index="<<index<<" val="<<treeSpace[index]<<endl;
+		if(lLimit!=rLimit) {
+			if(!(rIdx<ST_rightRangeLeft(lLimit,rLimit))) 
+				pushKernel(t,lIdx,rIdx,ST_rightRangeLeft(lLimit,rLimit),ST_rightRangeRight(lLimit,rLimit),ST_right(index),f);
+			if(!(lIdx>ST_leftRangeRight(lLimit,rLimit))) 
+				pushKernel(t,lIdx,rIdx,ST_leftRangeLeft(lLimit,rLimit),ST_leftRangeRight(lLimit,rLimit),ST_left(index),f);
+			//treeSpace[index]=f(treeSpace[index],);
+			treeSpace[index]=f(treeSpace[ST_left(index)],treeSpace[ST_right(index)]);
+		}else{
+			treeSpace[index]=f(treeSpace[index],t);
+		}
+		return treeSpace[index];
+	}
+	template <typename T>
+	T segmentTree<T>::getKernel(size_t lIdx,size_t rIdx,size_t lLimit,size_t rLimit,size_t index,T (*f)(T &lNode,T &rNode)){
+		//if(lIdx<=lLimit&&rIdx>=rLimit)
+		//cout<<" lIdx="<<lIdx<<" rIdx="<<rIdx<<" lLimit="<<lLimit<<" rLimit="<<rLimit<<" index="<<index<<" val="<<treeSpace[index]<<endl;
+		if(lLimit!=rLimit) {
+			if(rIdx>=rLimit&&lIdx<=lLimit) return treeSpace[index];
+			if(rIdx<ST_rightRangeLeft(lLimit,rLimit))
+				return getKernel(lIdx,rIdx,ST_leftRangeLeft(lLimit,rLimit),ST_leftRangeRight(lLimit,rLimit),ST_left(index),f);
+			if(lIdx>ST_leftRangeRight(lLimit,rLimit))
+				return getKernel(lIdx,rIdx,ST_rightRangeLeft(lLimit,rLimit),ST_rightRangeRight(lLimit,rLimit),ST_right(index),f);
+			T a=getKernel(lIdx,rIdx,ST_rightRangeLeft(lLimit,rLimit),ST_rightRangeRight(lLimit,rLimit),ST_right(index),f);
+			T b=getKernel(lIdx,rIdx,ST_leftRangeLeft(lLimit,rLimit),ST_leftRangeRight(lLimit,rLimit),ST_left(index),f);
+				
+			return f(a,b);
+		}
+		return treeSpace[index];
+	}
+}
+template <typename T>
+inline T maxify(T &a,T &b){
+	return max(a,b);
+}
+
+int main(){
+	skylarkgit::segmentTree<int> st(10);
+	int t=10;
+	while(t--) {int l=rand()%20;st.push(l,t,t,maxify);cout<<t<<"="<<l<<endl;}
+	t=10;
+	while(t--) cout<<"max(0,"<<t<<")="<<st.get(0,t,maxify)<<endl;
+	t=10;
+	while(t--) cout<<"max("<<t<<",9)="<<st.get(t,9,maxify)<<endl;
+}
