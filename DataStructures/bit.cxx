e7190719a7598b287bd64148d6fcc3be931a1331
skylarkgit <abhay199658@gmail.com>
2018-05-25 14:11:06 +0000

    binary indexed tree (fenwick tree) added

---
+++ DataStructures/bit.cxx
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+namespace skylarkgit{
+	#define llu long long unsigned	
+	#define PENDING 	true
+	#define COMPLETE	false
+	#define BIT_parent(x)	(((x)^((~x)+1))&x)
+	#define BIT_next(x)		(((x)&((~x)+1))+x)
+
+	template <typename T>
+	class fenwickTree{
+
+	protected:
+		size_t 		maxSize,maxUserSize;
+		size_t 		bitLength;
+		std::vector<T> 	treeSpace;
+	public:
+				fenwickTree(size_t size,T &defaultVal);
+		void 	update(T &t,size_t a,T (*f)(T &currNode,T &thisNode));
+		T 		get	(size_t a,T (*f)(T &currNode,T &thisNode));
+	};
+
+	template <typename T>
+	fenwickTree<T>::fenwickTree(size_t size,T &defaultVal){
+		maxUserSize=size;
+		maxSize=size;
+		treeSpace.resize(maxSize+1);
+		int t=maxSize+1;
+		while(t--) treeSpace[t]=defaultVal;
+	}
+
+	template <typename T>
+	void fenwickTree<T>::update(T &t,size_t a,T (*f)(T &currNode,T &thisNode)){
+		++a;
+		while(a<=maxSize){
+			treeSpace[a]=f(treeSpace[a],t);
+			a=BIT_next(a);
+		}
+	}
+
+	template <typename T>
+	T fenwickTree<T>::get(size_t a,T (*f)(T &currNode,T &thisNode)){
+		++a;
+		treeSpace[0]=treeSpace[a];
+		a=BIT_parent(a);
+		while(a){
+			treeSpace[0]=f(treeSpace[0],treeSpace[a]);
+			a=BIT_parent(a);
+		}
+		return treeSpace[0];
+	}
+}
+template <typename T>
+inline T sumify(T &a,T &b){
+	return a+b;
+}
+
+int main(){
+	int p=0;
+	skylarkgit::fenwickTree<int> ft(10,p);
+	int t=10;
+	while(t--) {int l=rand()%20;ft.update(l,t,sumify);cout<<" "<<l;}cout<<endl;
+	t=10;
+	while(t--) cout<<"sum(0,"<<t<<")="<<ft.get(t,sumify)<<endl;
+	
+}
